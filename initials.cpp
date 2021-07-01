4348b81651c8a584331d07cf39ac07a2c30fa8bb
Greenhamster23 <86371887+Greenhamster23@users.noreply.github.com>
2021-07-01 13:18:27 -0700

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+// g++ initials.cpp -o cwc.o
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << " GG   D     D   GG  \n";
+  cout << "G  G   D D D   G  G \n";
+  cout << "G       D D    G    \n";
+  cout << "G  G           G  G \n";
+  cout << " GG             GG  \n";
+  
+  return 0;
+}
